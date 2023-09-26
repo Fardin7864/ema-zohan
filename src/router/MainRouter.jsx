67fd9775d1d12829d44edbf6bd25f24453d1dfca
Fardin7864 <fardinhasan1888@gmail.com>
2023-09-27 00:29:52 +0600

    add banner on home page, create footer and error component

--- src/router/MainRouter.jsx
+++ src/router/MainRouter.jsx
@@ -1,10 +1,24 @@
+import Home from '../components/Home/Home';
 import Root from '../components/Root/Root'
 import { createBrowserRouter } from "react-router-dom";
+import Shop from '../components/Shop/Shop';
+import Error from '../components/Error/Error';
 
  const MainRouter = createBrowserRouter([
     {
         path: '/',
-        element: <Root></Root>
+        element: <Root></Root>,
+        errorElement:<Error></Error>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/shop',
+                element: <Shop></Shop>
+            }
+        ]
     }
 ])
 
