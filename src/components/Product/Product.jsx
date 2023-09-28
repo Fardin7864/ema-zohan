fa2e4eba289000c920a4f271ccebfcda265bc8ae
Fardin7864 <fardinhasan1888@gmail.com>
2023-09-28 11:33:03 +0600

    design cards

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,27 +1,40 @@
+import Rating from "react-rating";
+import {AiOutlineStar,AiFillStar} from "react-icons/ai"
 
-const Product = ({product}) => {
-    const {category, img, name, price, ratings, ratingsCount, seller, stock} = product
+const Product = ({ product }) => {
+  const { category, img, name, price, ratings, ratingsCount, seller, stock } =
+    product;
 
-    // console.log(product)
-    return (
-        <div className="w-[300px] h-[508px] shadow-xl">
-            <div className="w-[286px] h-[286px] rounded-lg p-5">
-                <img src={img} alt="" className="rounded-lg"/>
-            </div>
-            <div className="px-5 flex flex-col flex-grow">
-                <h5 className="text-[#0E161A] text-xl font-normal">{name}</h5>
-                <p className="text-base font-normal">Price: ${price}</p>
-            </div>
-            <div className="px-5 flex flex-col">
-                <p className="text-[#2A414F] text-xs font-normal">Manufacturer: {seller}</p>
-                <p>Rating: {ratings} Star</p>
-            </div>
-            <div className="w-full flex justify-center items-center h-12 bg-[#FFE0B3] rounded-br-lg rounded-bl-lg">
-                <button>Add to Cart </button>
-            </div>
-            
+  return (
+    <div className="card bg-base-100 shadow-xl">
+      <figure>
+        <img src={img} alt="Shoes" />
+      </figure>
+      <div className="card-body px-0 py-0 gap-3">
+        <div className="px-5 flex flex-col">
+          <h5 className="text-[#0E161A] text-xl font-normal">{name}</h5>
+          <p className="text-base font-normal">Price: ${price}</p>
         </div>
-    );
+        <div className="px-5 flex flex-col grow">
+          <p className="text-[#878888] text-xs font-normal">
+            Manufacturer: {seller}
+          </p>
+          <p className="flex justify-center items-center text-yellow-300 text-xl">
+          <Rating
+             emptySymbol={<AiOutlineStar></AiOutlineStar>}
+             fullSymbol={<AiFillStar></AiFillStar>}
+             initialRating={ratings}
+             readonly></Rating>
+            </p>
+        </div>
+        <div className="card-actions justify-end">
+        <div className="w-full flex justify-center items-center h-12 bg-[#FFE0B3] rounded-br-lg rounded-bl-lg">
+          <button>Add to Cart </button>
+        </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -5,7 +5,7 @@ const Products = () => {
     const products = useLoaderData();
     // console.log(products)
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 p-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 p-4 gap-7">
             {
                 products.map(product => 
                     <Product
