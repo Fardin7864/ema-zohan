bf7b8c448569d81ad1a1057e96512356fd1c9a3f
Fardin7864 <fardinhasan1888@gmail.com>
2023-09-29 16:50:18 +0600

    add email varifycation

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,39 +1,41 @@
-import { useState } from "react";
-import app from "../../fire-base/firebase.init";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  const [user, setUser] = useState(null);
-  const auth = getAuth(app);
-  const provider = new GoogleAuthProvider();
-  const handleLogin = () => {
-    signInWithPopup(auth, provider)
-      .then((user) => setUser(user.user))
-      .catch((err) => console.log(err.message));
-  };
-  const handleSignOut = () =>{
-    signOut(auth)
-    .then(result => setUser(null))
-    .catch(err => console.log(err.message))
-  }
-  const { displayName , email, photoURL } = user || {};
-console.log(user?.displayName, user?.email)
-
   return (
-    <div>
-        {
-          !user && <button onClick={handleLogin} className="btn">Google Login</button> || <button onClick={handleSignOut} className="btn">Sign Out</button> 
-        }
-
-      { user &&
-            <div>
-            <h5>Name: {displayName}</h5>
-            <p>E-mail: {email}</p>
-            <div>
-                <img src={photoURL} alt="" />
-            </div>
+    <div className="my-36 flex flex-col justify-center items-center ">
+      <div className="w-[451px] h-[451px] absolute rounded-lg -z-10 bg-[#FFE0B3]"></div>
+      <div className="w-[451px] h-[451px] rounded-lg z-10 relative -right-7 -top-7 bg-white border-2 border-[#95A0A7] py-5">
+        <div className="flex flex-col items-center w-full">
+          <h3 className="text-3xl">Log In</h3>
+          <div className="w-5/6 space-y-4">
+            <p className="text-base font-normal text-[#2A414F]">Email: </p>
+            <input
+              type="email"
+              name="email"
+              id=""
+              className="border-2 text-2xl border-[#95A0A7] rounded-md h-14 w-4/5"
+            />
+            <p className="text-base font-normal text-[#2A414F]">Password: </p>
+            <input
+              type="password"
+              name="password"
+              id=""
+              className="border-2 text-2xl border-[#95A0A7] rounded-md h-14 w-4/5"
+            />
+            <input
+              type="submit"
+              name="submit"
+              className="btn w-full bg-[#FFE0B3]"
+            />
+            <p className="text-base font-normal text-[#2A414F]">
+              New to this site ?{" "}
+              <Link to="/login" className="text-yellow-400">
+                Sign Up
+              </Link>
+            </p>
           </div>
-      }
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,58 +1,136 @@
 import { Link } from "react-router-dom";
-import {FcGoogle} from 'react-icons/fc';
-import {AiFillGithub} from 'react-icons/ai';
-import {BsFacebook} from 'react-icons/bs'
+import { FcGoogle } from "react-icons/fc";
+import { AiFillGithub } from "react-icons/ai";
+import { BsFacebook } from "react-icons/bs";
 
-import { GoogleAuthProvider, signInWithPopup, getAuth, GithubAuthProvider } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  getAuth,
+  GithubAuthProvider,
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+} from "firebase/auth";
 import app from "../../fire-base/firebase.init";
 import { useState } from "react";
 
-
-
 const SignIn = () => {
-    const [user, setUser] = useState(null);
-    const auth = getAuth(app);
+  const [user, setUser] = useState(null);
+  const [errorMsg, setErrorMsg] = useState();
+  const [successMsg, setSuccessMsg] = useState();
+  const auth = getAuth(app);
+  // setErrorMsg('');
+  // Google authantication
+  const provider = new GoogleAuthProvider();
+  const handleWithGoogle = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => setUser(result.user))
+      .catch((err) => console.log(err.message));
+  };
+  // Github authantication
+  const gitProvider = new GithubAuthProvider();
+  const handlewithGithub = () => {
+    signInWithPopup(auth, gitProvider)
+      .then((result) => setUser(result.user))
+      .catch((err) => console.log(err.message));
+  };
+  // Email authantication
+  // console.log(user);
 
-    // Google authantication
-    const provider = new GoogleAuthProvider()
-    const handleWithGoogle = () => { 
-        signInWithPopup(auth,provider)
-        .then(result => setUser(result.user))
-        .catch(err => console.log(err.message))
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrorMsg('');
+    setSuccessMsg('');
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    if (password.length < 6) {
+      
+      setErrorMsg('Password must be 6 charectar or more!')
+      return;
     }
+    else{
+      createUserWithEmailAndPassword(auth, email, password)
+      .then(result => {
+        setUser(result.user);
+        setSuccessMsg('Please check you email and veryfi it!')
+        sendEmailVerification(auth.currentUser)
+        .then(result => {
+          console.log(result, 'email send')
+        })
+        e.target.email.value = '';
+        e.target.password.value = '';
+      })
+      .catch(err => {
+        console.log(err.message)
+        e.target.password.value = '';
+      })  
 
-    // Github authantication
-    const gitProvider = new GithubAuthProvider();
-    const handlewithGithub = () => { 
-        signInWithPopup(auth,gitProvider)
-        .then(result => setUser(result.user))
-        .catch(err => console.log(err.message))
-     }
-    console.log(user)
-
+    }
+  };
+  console.log(user)
 
   return (
     <div className="my-36 flex flex-col justify-center items-center ">
       <div className="w-[430px] h-[633px] absolute rounded-lg -z-10 bg-[#FFE0B3]"></div>
       <div className="w-[451px] rounded-lg z-10 relative -right-7 -top-7 bg-white border-2 border-[#95A0A7] py-5">
-        <div className="flex flex-col items-center w-full">
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-col items-center w-full">
             <h3 className="text-3xl">Sign Up</h3>
             <div className="w-5/6 space-y-4">
-                <p className="text-base font-normal text-[#2A414F]">Email: </p>
-                <input type="email" name="" id=""  className="border-2 border-[#95A0A7] rounded-md h-14 w-4/5"/>
-                <p className="text-base font-normal text-[#2A414F]">Password: </p>
-                <input type="password" name="" id="" className="border-2 border-[#95A0A7] rounded-md h-14 w-4/5" />
-                <p className="text-base font-normal text-[#2A414F]">Confirm Password: </p>
-                <input type="password" name="" id=""  className="border-2 border-[#95A0A7] rounded-md h-14 w-4/5"/>
-                <button className="btn w-full bg-[#FFE0B3]">Log In</button>
-                <p className="text-base font-normal text-[#2A414F]">Already have account? <Link to='/login/login' className="text-yellow-400">Login</Link></p>
-                <div className="flex justify-center items-center"><p>or</p></div>
-                <button onClick={handleWithGoogle} className="btn w-full bg-"><FcGoogle className="text-3xl"></FcGoogle> Continue with Google</button>
-                <button onClick={handlewithGithub} className="btn w-full bg-"><AiFillGithub className="text-3xl"></AiFillGithub> Continue with Git-hub</button>
-                <button className="btn w-full bg-"><BsFacebook className="text-3xl"></BsFacebook> Continue with Facebook</button>
+              <p className="text-base font-normal text-[#2A414F]">Email: </p>
+              <input
+                type="email"
+                name="email"
+                id=""
+                required
+                className="border-2 text-2xl border-[#95A0A7] rounded-md h-14 w-4/5"
+              />
+              <p className="text-base font-normal text-[#2A414F]">Password: </p>
+              <input
+                type="password"
+                name="password"
+                id=""
+                required
+                className="border-2 text-2xl border-[#95A0A7] rounded-md h-14 w-4/5"
+              />
+              {
+                errorMsg && <p className="text-base font-normal text-red-600">{errorMsg}</p> || ''
 
+              }
+              {
+                successMsg && <p className="text-base font-normal text-green-600">{successMsg}</p> || ''
+
+              }
+                {/* <input type="password" name="" id=""  className="border-2 text-2xl border-[#95A0A7] rounded-md h-14 w-4/5"/> */}
+              <input
+                type="submit"
+                name="submit"
+                value="submit"
+                className="btn w-full bg-[#FFE0B3]"
+              />
+              <p className="text-base font-normal text-[#2A414F]">
+                Already have account?{" "}
+                <Link to="/login/login" className="text-yellow-400">
+                  Login
+                </Link>
+              </p>
+              <div className="flex justify-center items-center">
+                <p>or</p>
+              </div>
+              <button onClick={handleWithGoogle} className="btn w-full bg-">
+                <FcGoogle className="text-3xl"></FcGoogle> Continue with Google
+              </button>
+              <button onClick={handlewithGithub} className="btn w-full bg-">
+                <AiFillGithub className="text-3xl"></AiFillGithub> Continue with
+                Git-hub
+              </button>
+              <button className="btn w-full bg-">
+                <BsFacebook className="text-3xl"></BsFacebook> Continue with
+                Facebook
+              </button>
             </div>
-        </div>
+          </div>
+        </form>
       </div>
     </div>
   );
